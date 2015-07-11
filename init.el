6f2da42935e5409842d16527775380fb939bc43e
redrobohood <redrobohood@users.noreply.github.com>
2015-07-11 11:21:27 -0400

    Add markdown-mode

--- init.el
+++ init.el
@@ -1,3 +1,8 @@
 (add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
 (evil-mode 1)
+
+(add-to-list 'load-path "~/.emacs.d/markdown-mode")
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
 cd ~/.emacs.d
 hg clone https://bitbucket.org/lyro/evil
+git clone git://jblevins.org/git/markdown-mode.git
