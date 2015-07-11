bd5044f1b9724d560fcd54c758e025d9ec3ac3d9
redrobohood <redrobohood@users.noreply.github.com>
2015-07-11 11:30:27 -0400

    Comment init.el

--- init.el
+++ init.el
@@ -1,7 +1,9 @@
+;; Evil
 (add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
 (evil-mode 1)
 
+;; markdown-mode
 (add-to-list 'load-path "~/.emacs.d/markdown-mode")
 (autoload 'markdown-mode "markdown-mode"
   "Major mode for editing Markdown files" t)
