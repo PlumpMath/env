854cda159bceb5cd017cce86535046723ee02aaf
redrobohood <redrobohood@users.noreply.github.com>
2015-07-13 23:00:00 -0400

    Add commands to set the Solarized background

--- init.el
+++ init.el
@@ -17,6 +17,18 @@
 ;; Solarized
 (add-to-list 'custom-theme-load-path "~/.emacs.d/emacs-color-theme-solarized")
 (load-theme 'solarized t)
+(defun set-frame-background (mode)
+  "Set the current frame's Solarized background to MODE."
+  (set-frame-parameter nil 'background-mode mode)
+  (enable-theme 'solarized))
+(defun light ()
+  "Use the Solarized light background in the current frame."
+  (interactive)
+  (set-frame-background 'light))
+(defun dark ()
+  "Use the Solarized dark background in the current frame."
+  (interactive)
+  (set-frame-background 'dark))
 
 ;; markdown-mode
 (add-to-list 'load-path "~/.emacs.d/markdown-mode")
