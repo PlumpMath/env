c76349b43c7631c4bd17be2b1b579ce495ab4045
elogent <elogent@users.noreply.github.com>
2015-09-18 09:32:19 -0400

    Simplify markdown-mode usage

--- init.el
+++ init.el
@@ -55,6 +55,4 @@ See `set-frame-background' for more details."
 
 ;; markdown-mode
 (add-to-list 'load-path "~/.emacs.d/markdown-mode")
-(autoload 'markdown-mode "markdown-mode"
-  "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(require 'markdown-mode)
