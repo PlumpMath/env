31f01f537b9b5930f904ec9474d26ff9a78a480d
elogent <elogent@users.noreply.github.com>
2016-04-21 15:00:21 -0400

    Add Git user.email and user.name config

--- env.sh
+++ env.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 git config --global core.excludesFile "$PWD/gitignore"
+git config --global user.email elogent@users.noreply.github.com
+git config --global user.name elogent
 
 rm ~/.emacs.el
 ln -s "$PWD/emacs.el" ~/.emacs.el
