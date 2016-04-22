c67e5b5755a1b718d64abcb64c9ed548a2b2dfe5
elogent <elogent@users.noreply.github.com>
2016-04-22 18:58:34 -0400

    Add Spotify

--- env.sh
+++ env.sh
@@ -10,3 +10,8 @@ rm ~/.emacs.el
 ln -s "$PWD/emacs.el" ~/.emacs.el
 
 apt-get install keepassx
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+echo deb http://repository.spotify.com stable non-free | tee /etc/apt/sources.list.d/spotify.list
+apt-get update
+apt-get install spotify-client
