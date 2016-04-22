041ab3a88851e7832cd0f9f362edc2be9bd051b5
elogent <elogent@users.noreply.github.com>
2016-04-22 19:14:17 -0400

    Use ln -f instead of rm

--- env.sh
+++ env.sh
@@ -6,8 +6,7 @@ git config --global user.email elogent@users.noreply.github.com
 git config --global user.name elogent
 
 apt-get install emacs
-rm ~/.emacs.el
-ln -s "$PWD/emacs.el" ~/.emacs.el
+ln -fs "$PWD/emacs.el" ~/.emacs.el
 
 apt-get install keepassx
 
