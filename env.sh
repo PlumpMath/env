5a56724b097cfb743e71c45ec7784d36772e1acb
elogent <elogent@users.noreply.github.com>
2016-04-23 11:57:30 -0400

    Use sudo in env.sh

--- env.sh
+++ env.sh
@@ -5,17 +5,17 @@ git config --global core.excludesFile "$PWD/gitignore"
 git config --global user.email elogent@users.noreply.github.com
 git config --global user.name elogent
 
-apt-get install emacs
+sudo apt-get install emacs
 ln -fs "$PWD/emacs.el" ~/.emacs.el
 
-rm /etc/apt/sources.list.d/webupd8team-ubuntu-java-*
-add-apt-repository ppa:webupd8team/java
-apt-get update
-apt-get install oracle-java8-installer
+sudo rm /etc/apt/sources.list.d/webupd8team-ubuntu-java-*
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
 
-apt-get install keepassx
+sudo apt-get install keepassx
 
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
-echo deb http://repository.spotify.com stable non-free | tee /etc/apt/sources.list.d/spotify.list
-apt-get update
-apt-get install spotify-client
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update
+sudo apt-get install spotify-client
