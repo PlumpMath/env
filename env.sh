59ad46c4ecd1334af50857da140e1b31e0da6f95
elogent <elogent@users.noreply.github.com>
2016-04-23 11:19:59 -0400

    Add Java

--- env.sh
+++ env.sh
@@ -8,6 +8,11 @@ git config --global user.name elogent
 apt-get install emacs
 ln -fs "$PWD/emacs.el" ~/.emacs.el
 
+rm /etc/apt/sources.list.d/webupd8team-ubuntu-java-*
+add-apt-repository ppa:webupd8team/java
+apt-get update
+apt-get install oracle-java8-installer
+
 apt-get install keepassx
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
