c7318c3d47c42aea8b9ff8a78221dce99d6e8f02
elogent <elogent@users.noreply.github.com>
2016-04-25 11:03:53 -0400

    Use apt instead of apt-get
    Ubuntu 16.04 suggests the use of apt instead of apt-get:
    http://ubuntuforums.org/showthread.php?t=2319347

--- env.sh
+++ env.sh
@@ -5,13 +5,13 @@ git config --global core.excludesFile "$PWD/gitignore"
 git config --global user.email elogent@users.noreply.github.com
 git config --global user.name elogent
 
-sudo apt-get install emacs
+sudo apt install emacs
 ln -fs "$PWD/emacs.el" ~/.emacs.el
 
 sudo rm /etc/apt/sources.list.d/webupd8team-ubuntu-java-*
 sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
+sudo apt update
+sudo apt install oracle-java8-installer
 
 sudo curl -Lo /usr/local/bin/boot https://github.com/boot-clj/boot-bin/releases/download/latest/boot.sh
 sudo chmod +x /usr/local/bin/boot
@@ -20,9 +20,9 @@ echo BOOT_CLOJURE_VERSION=1.8.0 > ~/.boot/boot.properties
 echo BOOT_EMIT_TARGET=no >> ~/.boot/boot.properties
 boot -u
 
-sudo apt-get install keepassx
+sudo apt install keepassx
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update
-sudo apt-get install spotify-client
+sudo apt update
+sudo apt install spotify-client
