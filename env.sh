cac9ee903d18ec5d7130af0ec3e12c4ffb651c4a
Sam Estep <sam@samestep.com>
2017-03-13 15:37:20 -0400

    Add Atom Advanced Open File package

--- env.sh
+++ env.sh
@@ -16,7 +16,7 @@ ln -fs "$PWD/init.el" ~/.emacs.d/init.el
 
 wget -O https://github.com/atom/atom/releases/download/v1.15.0/atom-amd64.deb
 sudo dpkg -i atom-amd64.deb
-mkdir ~/.atom
+apm install advanced-open-file@0.16.6
 ln -fs "$PWD/config.cson" ~/.atom/config.cson
 ln -fs "$PWD/keymap.cson" ~/.atom/keymap.cson
 
